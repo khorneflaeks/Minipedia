3b14cffb218471467d4bba3a2a391648b1f9c7f0
Pugalotl <94671281+Pugalotl@users.noreply.github.com>
2022-02-18 10:43:46 +1100

    Add files via upload

--- encoder.py
+++ encoder.py
@@ -1,7 +1,7 @@
 from bitstring import BitArray
 import json
 import requests
-
+charlist = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.',',','(',')']
 
 def bitify(inputs):
     binry = ""
@@ -33,6 +33,10 @@ def wikifind(webname):
     wikitext = x.json()
     for k,item in wikitext["query"]["pages"].items():
         return item['extract']
+        
+def remover(text):
+
+    ''.join([i for i in text if i in aminoacids])
     
 
 strings = input("Enter a string: ")
