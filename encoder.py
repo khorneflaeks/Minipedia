e864a02955dae78aab096208fb69988eba8bc641
Pugalotl <94671281+Pugalotl@users.noreply.github.com>
2022-02-19 14:37:43 +1100

    deleted unnecessary include

--- encoder.py
+++ encoder.py
@@ -3,7 +3,6 @@ import json
 import requests
 import re
 import unidecode
-from num2words import num2words
 
 charlist = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.',',','(',')']
 
